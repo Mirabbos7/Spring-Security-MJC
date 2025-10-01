9fd972d9bc0ad300a23fa448471ae6bc3b555908
Mirabbos7 <mirabbosahmedov7777@gmail.com>
2025-10-01 18:42:41 +0500

    changed to a record

--- module-service/src/main/java/com/mjc/school/dto/NewsPageDtoResponse.java
+++ module-service/src/main/java/com/mjc/school/dto/NewsPageDtoResponse.java
@@ -2,28 +2,6 @@ package com.mjc.school.dto;
 
 import java.util.List;
 
-public class NewsPageDtoResponse {
-    private List<NewsDtoResponse> newsList;
-    private long totalNewsCount;
+public record NewsPageDtoResponse(List<NewsDtoResponse> newsList, long totalNewsCount) {
 
-    public NewsPageDtoResponse(List<NewsDtoResponse> newsList, long totalNewsCount) {
-        this.newsList = newsList;
-        this.totalNewsCount = totalNewsCount;
-    }
-
-    public List<NewsDtoResponse> getNewsList() {
-        return newsList;
-    }
-
-    public void setNewsList(List<NewsDtoResponse> newsList) {
-        this.newsList = newsList;
-    }
-
-    public long getTotalNewsCount() {
-        return totalNewsCount;
-    }
-
-    public void setTotalNewsCount(long totalNewsCount) {
-        this.totalNewsCount = totalNewsCount;
-    }
 }
